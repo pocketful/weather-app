348c95aa3652ab8101f8b8c8b7e3ed0ff3dd3fa9
pocketful <iveta.jac@gmail.com>
2022-11-19 23:09:23 +0200

    dateFormatted

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -6,18 +6,28 @@ const getIcon = (icon) =>
   `https://developer.accuweather.com/sites/default/files/${icon}-s.png`;
 
 // TODO: Implement weather card based on the design from the screenshot in the instructions.
-export const WeatherCard = (props) => (
-  <div className="weather_card" data-testid="weather_card">
-    {/* Implement under this line ⬇️ */}
-    <p className="card_date">{new Date(props.date).toDateString()}</p>
-    <p className="card_summary">{props.summary}</p>
-    <div className="card_temp">
-      <div>
-        <p>Temperatures:</p>
-        <p>min {props.minTemp}</p>
-        <p>max {props.maxTemp}</p>
+export const WeatherCard = (props) => {
+  const options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  };
+  const date = new Date(props.date);
+  const dateFormatted = date.toLocaleDateString('en-GB', options);
+
+  return (
+    <div className="weather_card" data-testid="weather_card">
+      {/* Implement under this line ⬇️ */}
+      <p className="card_date">{dateFormatted}</p>
+      <p className="card_summary">{props.summary}</p>
+      <div className="card_temp">
+        <div>
+          <p>Temperatures:</p>
+          <p>min {props.minTemp}</p>
+          <p>max {props.maxTemp}</p>
+        </div>
       </div>
-      <img src="https://developer.accuweather.com/sites/default/files/01-s.png" alt="weather icon" />
     </div>
-  </div>
-);
+  );
+};
